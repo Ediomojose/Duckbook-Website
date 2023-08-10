8031cf8803c52269d2625362c4d8d16a3397521f
Ediomojose.Dev <goodnewsediomojoshua@yahoo.com>
2023-08-10 22:34:47 +0100

    Goals section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import AboutSection from './components/AboutSection'
 import BrandTrustSection from './components/BrandTrustSection'
+import GoalSection from './components/GoalSection'
 import HeroSection from './components/HeroSection'
 import Nav from './components/Nav'
 
@@ -12,6 +13,7 @@ function App() {
     <HeroSection/>
     <BrandTrustSection/>
     <AboutSection/>
+    <GoalSection/>
     </>
   )
 }
---
+++ src/components/GoalSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Avatar1 from "/public/images/Ellipse 1.png";
+import Avatar2 from "/public/images/Ellipse 2.png";
+import Avatar3 from "/public/images/Ellipse 3.png";
+
+const GoalSection = () => {
+  return (
+    <>
+      <aside className="bg-natural-orange  flex flex-col items-center justify-center gap-8 px-10 py-8 md:flex-row md:px-20 md:py-12 lg:px-32 lg:py-16 2xl:px-42">
+        <div className="grid grid-cols-2 items-center justify-center">
+          <img src={Avatar1} alt="Avatar" className="row-span-3"/>
+          <img src={Avatar3} alt="Avatar" />
+          <img src={Avatar2} alt="Avatar" />
+        </div>
+        <p className="text-natural-black font-bold font-avenir lg:text-3xl">
+          Duckbook is determined to helped over 1,000,000,000 Nigerians small
+          business owners take control of their finances
+        </p>
+      </aside>
+    </>
+  );
+};
+
+export default GoalSection;
