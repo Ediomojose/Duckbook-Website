d7e6548cb45f8ed666e96b1921fa41bda2b02074
Ediomojose.Dev <goodnewsediomojoshua@yahoo.com>
2023-08-11 11:51:23 +0100

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import AboutSection from './components/AboutSection'
 import BrandTrustSection from './components/BrandTrustSection'
+import Footer from './components/Footer'
 import GoalSection from './components/GoalSection'
 import HeroSection from './components/HeroSection'
 import Nav from './components/Nav'
@@ -14,6 +15,7 @@ function App() {
     <BrandTrustSection/>
     <AboutSection/>
     <GoalSection/>
+    <Footer/>
     </>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,15 @@ import BrandLogo from "./BrandLogo";
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div>
+      <footer className="px-8 py-20 grid grid-cols-1 items-center justify-center gap-10">
+        <div className="flex flex-col justify-center items-center gap-1">
           <h1 className="font-silk text-natural-green text-4xl text-center md:text-7xl lg:text-8xl">
             Manage your money like a boss
           </h1>
-          <p>Join 100+ small businesses who have chosen duckbook.</p>
+          <p className="text-center">Join 100+ small businesses who have chosen duckbook.</p>
           <Button />
         </div>
-        <div>
+        <div className="grid grid-cols-2 justify-between items-start gap-40 ">
           <BrandLogo />
           <ul>
             <li>Company</li>
@@ -28,7 +28,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <p>Duckbook 2023, All Rights Reserved.</p>
+        <p className="text-center">Duckbook 2023, All Rights Reserved.</p>
       </footer>
     </>
   );
